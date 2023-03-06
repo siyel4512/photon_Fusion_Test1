30f6f7b95404add005d6fcd245eb3c13248276ad
SANGHALEE <sangha206@naver.com>
2023-03-06 17:00:28 +0900

    Animation Test2

--- Assets/02.Scripts/WIndmill/Windmill2.cs
+++ Assets/02.Scripts/WIndmill/Windmill2.cs
@@ -1,26 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
-public class Windmill2 : MonoBehaviour
+public class Windmill2 : NetworkBehaviour
 {
     private Animator anim;
     private int start_id;
+    private bool isPlaying = false;
+    [Networked] public bool S { get; set; }
 
     // Start is called before the first frame update
     void Start()
     {
+        print("Windmill2 Start() 실행");
         anim = GetComponent<Animator>();
         start_id = Animator.StringToHash("Start");
+        anim.speed = 0f;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void Render()
     {
+        anim.SetBool(start_id, true);
+
         if (Input.GetKeyDown(KeyCode.R))
         {
-            print("애니메이션 재생");
-            anim.SetBool(start_id, true);
+            if (isPlaying)
+            {
+                isPlaying = false;
+                print("애니메이션 중지");
+                //anim.SetBool(start_id, isPlaying);
+                anim.speed = 0f;
+            }
+            else
+            {
+                isPlaying = true;
+                print("애니메이션 재생");
+                //anim.SetBool(start_id, isPlaying);
+                anim.speed = 1f;
+            }
         }
     }
 }
